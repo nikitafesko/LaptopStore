33b068de3c8790e0db4246bbfe1b17d9d825c9e6
Nikita <58188102+NickDesire@users.noreply.github.com>
2020-09-07 18:09:21 +0300

    Add files via upload

---
+++ Startup.cs
@@ -0,0 +1,55 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Shop.Data;
+using Shop.Data.Intefaces;
+using Shop.Data.mocks;
+using Shop.Data.Repository;
+
+namespace Shop
+{
+    public class Startup
+    {
+        private IConfigurationRoot _confSting;
+
+        public Startup(IWebHostEnvironment hostEnv)
+        {
+            _confSting = new ConfigurationBuilder().SetBasePath(hostEnv.ContentRootPath).AddJsonFile("dbsettings.json").Build();
+        }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<AppDBContent>(options => options.UseSqlServer(_confSting.GetConnectionString("DefaultConnection"))); //подключение бд
+            
+            services.AddTransient<IAllLaptops, LaptopRepository>();
+            services.AddTransient<ILaptopsCategory, CategoryRepository>();
+            
+            services.AddMvc(options => options.EnableEndpointRouting = false);
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+
+            app.UseDeveloperExceptionPage();
+            app.UseStatusCodePages();
+            app.UseStaticFiles();
+            app.UseMvcWithDefaultRoute();
+           
+            using (var scope = app.ApplicationServices.CreateScope())
+            {
+                AppDBContent content = scope.ServiceProvider.GetRequiredService<AppDBContent>();
+                DBObjects.Initial(content);
+            }
+        }
+    }
+}
